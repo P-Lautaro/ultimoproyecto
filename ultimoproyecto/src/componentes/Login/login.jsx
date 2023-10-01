79f3aac59dbf901cdebd090da186a8399b71e230
P-Lautaro <parritalauti@gmail.com>
2023-10-01 19:14:25 -0300

    agregando import de emaillpng

--- ultimoproyecto/src/componentes/Login/login.jsx
+++ ultimoproyecto/src/componentes/Login/login.jsx
@@ -3,6 +3,8 @@ import './login.css'
 
 import user_icon from '../Assets/email.png'
 import password_icon from '../Assets/password.png'
+import email_icon from '../Assets/email.png'
+
 const login = () =>{
    return (
     <div>
