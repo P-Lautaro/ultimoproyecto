df65786813d972353eb4218b205dabbef42fbff6
P-Lautaro <parritalauti@gmail.com>
2023-10-01 19:12:29 -0300

    agregando import de passwordpng

--- ultimoproyecto/src/componentes/Login/login.jsx
+++ ultimoproyecto/src/componentes/Login/login.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './login.css'
 
 import user_icon from '../Assets/email.png'
-
+import password_icon from '../Assets/password.png'
 const login = () =>{
    return (
     <div>
