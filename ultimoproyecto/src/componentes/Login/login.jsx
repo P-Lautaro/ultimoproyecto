f4bf73774d95e66b19a63fc7cef51d8c4f47c7c7
P-Lautaro <parritalauti@gmail.com>
2023-10-01 19:15:38 -0300

    correccion en import user

--- ultimoproyecto/src/componentes/Login/login.jsx
+++ ultimoproyecto/src/componentes/Login/login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './login.css'
 
-import user_icon from '../Assets/email.png'
+import user_icon from '../Assets/person.png'
 import password_icon from '../Assets/password.png'
 import email_icon from '../Assets/email.png'
 
