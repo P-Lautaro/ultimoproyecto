c142d49c5882ae3f81698063856a5047c6c76201
P-Lautaro <parritalauti@gmail.com>
2023-10-02 15:16:34 -0300

    eliminando

--- ultimoproyecto/src/App.js
+++ ultimoproyecto/src/App.js
@@ -18,7 +18,6 @@ function App() {
         <ClipLoader
           color={color}
           loading={loading}
-          cssOverride={override}
           size={150}
           aria-label="Loading Spinner"
           data-testid="loader"
