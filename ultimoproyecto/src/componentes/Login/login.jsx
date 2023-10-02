3043fd1c047793bfeb4c3ca76b4f20bf241a46c2
P-Lautaro <parritalauti@gmail.com>
2023-10-02 12:45:42 -0300

    agregando action en login

--- ultimoproyecto/src/componentes/Login/login.jsx
+++ ultimoproyecto/src/componentes/Login/login.jsx
@@ -37,7 +37,7 @@ const login = () =>{
       <div className="forgot-password"> Lost Password? <span>Click Here!</span></div>
       <div className="submit-container">
         <div className={action==="Login"?"submit gray":"submit"}>Sign Up</div>
-        <div className="submit">Login</div>
+        <div className={action==="Sign Up"?"submit gray":"submit"}>Login</div>
       </div>
     </div>
    );
