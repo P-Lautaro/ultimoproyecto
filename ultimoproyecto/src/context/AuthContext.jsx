9312374c6de4ecab5e8ecae706f7984fe10f5bf2
P-Lautaro <parritalauti@gmail.com>
2023-10-08 20:42:01 -0300

    agregando export const

--- ultimoproyecto/src/context/AuthContext.jsx
+++ ultimoproyecto/src/context/AuthContext.jsx
@@ -3,3 +3,11 @@ import { Auth } from "../firebase/firebase.config";
 import { createContext, useContext } from "react";
 
 export const authContext = createContext();
+
+export const useAuth = () => {
+    const context = useContext(authContext);
+    if (!context) {
+        console.log("error creating auth context")
+    }
+    return context;
+};
