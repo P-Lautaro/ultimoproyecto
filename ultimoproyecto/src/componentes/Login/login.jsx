2b3143c22fa0fff70f03f2f8942ab373b9f7114b
P-Lautaro <parritalauti@gmail.com>
2023-10-08 22:04:07 -0300

    modificando

--- ultimoproyecto/src/componentes/Login/login.jsx
+++ ultimoproyecto/src/componentes/Login/login.jsx
@@ -9,8 +9,6 @@ import email_icon from '../Assets/email.png';
 const Login = () =>{
 
    const [action, setAction] = useState("Login");
-  
-  
 
    return (
     <div className="container">
@@ -41,8 +39,14 @@ const Login = () =>{
       {action==="Sign Up"?<div></div>:<div className="forgot-password"> Lost Password? <span>Click Here!</span></div>} 
       
       <div className="submit-container">
-        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
-        <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+      <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => {
+  console.log("Botón Sign Up clickeado");
+  setAction("Sign Up");
+}}>Sign Up</div>
+<div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => {
+  console.log("Botón Login clickeado");
+  setAction("Login");
+}}>Login</div>
       </div>
     </div>
    );
