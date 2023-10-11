844f970ed69ec8d0943cda7e8384fa2ff398b9e8
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-10-11 09:01:54 -0300

    Export fuction Nav

--- ultimoproyecto/src/componentes/Nav/nav.jsx
+++ ultimoproyecto/src/componentes/Nav/nav.jsx
@@ -3,6 +3,15 @@ import "./nav.css";
 
 export function Nav (props){
     return(
-        <div className={`barra ${isBoxVisible ? "active" : ""}`}></div>
+        <div className={`barra ${isBoxVisible ? "active" : ""}`}>
+            <nav>
+        <div className="titulo2-container">
+            <h1 className="titulos-botones">{props.Infoh1}</h1>
+        </div>
+        <div className="icon-container" onClick={handleIconClick}>
+          <img src={myImage} alt="Profile" className="icon" />
+        </div>
+      </nav>
+        </div>
     )
 }
