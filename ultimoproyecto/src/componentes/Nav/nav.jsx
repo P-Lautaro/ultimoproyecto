1eac31290b9c5630b857813a83dde111bda63be9
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-10-11 09:01:08 -0300

    Export fuction Nav

--- ultimoproyecto/src/componentes/Nav/nav.jsx
+++ ultimoproyecto/src/componentes/Nav/nav.jsx
@@ -2,5 +2,7 @@ import React, { useState } from "react";
 import "./nav.css";
 
 export function Nav (props){
-    
+    return(
+        <div className={`barra ${isBoxVisible ? "active" : ""}`}></div>
+    )
 }
