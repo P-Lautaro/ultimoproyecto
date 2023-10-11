742d4f68c09552eded78d3c0506b16aeb039527a
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-10-11 19:18:20 -0300

    clistado.js

--- ultimoproyecto/src/componentes/clistado.js
+++ ultimoproyecto/src/componentes/clistado.js
@@ -5,7 +5,8 @@ import { Footer } from "./Footer/footer";
 export default function Cistado (){
     return (
         <div>
-            
+            <Nav Infoh1="Asistencia de Alumnos"/>
+            <Footer/>
         </div>
     )
 }
