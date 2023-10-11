a4ef1cd4c095be6794feae20b0c755c9250eb791
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-10-11 19:17:35 -0300

    clistado.js

--- ultimoproyecto/src/componentes/clistado.js
+++ ultimoproyecto/src/componentes/clistado.js
@@ -2,3 +2,10 @@ import React, { Component, useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { Nav } from "./Nav/nav";
 import { Footer } from "./Footer/footer";
+export default function Cistado (){
+    return (
+        <div>
+            
+        </div>
+    )
+}
