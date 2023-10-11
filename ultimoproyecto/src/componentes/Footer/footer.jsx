f36a295776ae41aad66a385ad96a1e8dc52a882b
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-10-11 18:32:31 -0300

    Return Footer P

--- ultimoproyecto/src/componentes/Footer/footer.jsx
+++ ultimoproyecto/src/componentes/Footer/footer.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useRef, useEffect } from "react";
 export function Footer () {
     return(
-        <footer></footer>
+        <footer>
+            <div>
+                <a href="https://epet20.com.ar/index.html">E.P.E.T N° 20</a>
+            </div>
+        </footer>
     )
 }
