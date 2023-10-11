c4f887b730d5310545ea20fe1f51ec050f2acf43
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-10-11 18:37:00 -0300

    Footer ClassName

--- ultimoproyecto/src/componentes/Footer/footer.jsx
+++ ultimoproyecto/src/componentes/Footer/footer.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 export function Footer () {
     return(
-        <footer>
+        <footer className="footer">
             <div>
                 <a href="https://epet20.com.ar/index.html">E.P.E.T N° 20</a>
             </div>
