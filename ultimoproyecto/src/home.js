61348e297e09dcb865201abfc768c42c1d73e0ad
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-10-11 09:03:46 -0300

    Export default fuction Home

--- ultimoproyecto/src/home.js
+++ ultimoproyecto/src/home.js
@@ -1,4 +1,8 @@
 import React, { Component, useEffect, useState } from "react";
 import ReactDOM from 'react-dom/client'
 import './style/home.css'
-import { Nav } from './componentes/Nav/nav.jsx'
+import { Nav } from './componentes/Nav/nav.jsx';
+
+export default function Home () {
+    
+}
