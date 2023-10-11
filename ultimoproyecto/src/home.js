d0b12a65df2fa5bc79061c904e8b5e5c249b51c4
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-10-11 18:39:30 -0300

    Import Footer-Home

--- ultimoproyecto/src/home.js
+++ ultimoproyecto/src/home.js
@@ -2,11 +2,12 @@ import React, { Component, useEffect, useState } from "react";
 import ReactDOM from 'react-dom/client'
 import './home.css';
 import { Nav } from "./componentes/Nav/nav";
-
+import { Footer } from "./componentes/Footer/footer";
 export default function Home () {
     return (
         <div>
             <Nav Infoh1="Asistencia de Alumnos"/>
+            <Footer/>
         </div>
     )
 }
