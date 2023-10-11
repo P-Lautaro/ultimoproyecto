922f522c49fd6085c9c8272eee50e032165c7ff5
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-10-11 09:02:43 -0300

    Export fuction Nav orden

--- ultimoproyecto/src/componentes/Nav/nav.jsx
+++ ultimoproyecto/src/componentes/Nav/nav.jsx
@@ -5,13 +5,13 @@ export function Nav (props){
     return(
         <div className={`barra ${isBoxVisible ? "active" : ""}`}>
             <nav>
-        <div className="titulo2-container">
-            <h1 className="titulos-botones">{props.Infoh1}</h1>
-        </div>
-        <div className="icon-container" onClick={handleIconClick}>
-          <img src={myImage} alt="Profile" className="icon" />
-        </div>
-      </nav>
+                <div className="titulo2-container">
+                    <h1 className="titulos-botones">{props.Infoh1}</h1>
+                </div>
+                <div className="icon-container" onClick={handleIconClick}>
+                    <img src={myImage} alt="Profile" className="icon" />
+                </div>
+            </nav>
         </div>
     )
 }
