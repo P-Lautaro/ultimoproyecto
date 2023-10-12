eea70550471e5b94dec4ec81f82eb21dad3aae25
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-10-12 19:07:27 -0300

    Modificar Listado Home

--- ultimoproyecto/src/home.js
+++ ultimoproyecto/src/home.js
@@ -4,7 +4,7 @@ import './home.css';
 import { Nav } from "./componentes/Nav/nav";
 import { Footer } from "./componentes/Footer/footer";
 import { CrearListado } from "./componentes/Crear Listado/crearListado";
-
+import { ModificarListado } from "./componentes/Modificar Listado/ModificarListado";
 export default function Home () {
     return (
         <div>
