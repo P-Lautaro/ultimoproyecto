e99f20f08ea9b9f33644b6ae02ee4a69767f8dbc
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-10-12 19:04:18 -0300

    Export Modificar Listado JSX

--- ultimoproyecto/src/componentes/Modificar Listado/ModificarListado.jsx
+++ ultimoproyecto/src/componentes/Modificar Listado/ModificarListado.jsx
@@ -2,3 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import './modificarListado.css'
 import { Link } from "react-router-dom";
+
+export function ModificarListado () {
+    return (
+        <Link to="/" className=""><p>Modificar Listado</p></Link>
+    )
+}
