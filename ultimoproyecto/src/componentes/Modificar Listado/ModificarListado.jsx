98e399a4a9fbed6e7db9d63efbf904bd753152be
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-10-12 19:09:39 -0300

    Modificar Listado CSS

--- ultimoproyecto/src/componentes/Modificar Listado/ModificarListado.jsx
+++ ultimoproyecto/src/componentes/Modificar Listado/ModificarListado.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import './modificarListado.css'
+import './modifcarListado.css'
 import { Link } from "react-router-dom";
 
 export function ModificarListado () {
