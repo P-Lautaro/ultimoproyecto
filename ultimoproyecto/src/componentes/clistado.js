783d71d1e173fa77542998cc665081696faa32f7
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-10-12 18:27:19 -0300

    clistado.js

--- ultimoproyecto/src/componentes/clistado.js
+++ ultimoproyecto/src/componentes/clistado.js
@@ -1,15 +1,12 @@
 import React, { Component, useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { Nav } from "./Nav/nav";
-import { Footer } from "./Footer/footer";
-import { CrearListado } from "./Crear Listado/crearListado";
 
 export default function Cistado (){
     return (
         <div>
             <Nav Infoh1="Asistencia de Alumnos"/>
-            <Footer/>
-            <CrearListado/>
+            
         </div>
     )
 }
