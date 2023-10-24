400a49fc1b42190b591ba9ee83cfbaf2bda4e863
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-10-24 11:04:05 -0300

    Register JS Route

--- ultimoproyecto/src/App.js
+++ ultimoproyecto/src/App.js
@@ -25,6 +25,7 @@ function App() {
           <Route path="/Home" element={<Home/>} />
           <Route path="/Crear-Listado" element={<Cistado/>}/>
           <Route path="/Tomar-Asistencia" element={<MListado/>}/>
+          <Route path="/Registrase" element={<Register/>} />
       </Routes>
     </Router>
   );
