13dd9591ba7a7c2176006108b0966beeb3fb107e
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-10-24 09:11:09 -0300

    Crear Listado JS

--- ultimoproyecto/src/componentes/clistado.js
+++ ultimoproyecto/src/componentes/clistado.js
@@ -1,101 +1,110 @@
-import React, { Component, useEffect, useState } from "react";
-import ReactDOM from "react-dom/client";
+import React, { useEffect, useState } from "react";
 import { Nav } from "./Nav/nav";
-import './Crear Listado/CrearLstado.css'
-export default function Cistado (){
-    const [alumnos, setAlumnos] = useState([]);
-    const [nuevoAlumno, setNuevoAlumno] = useState({
+import './Crear Listado/CrearLstado.css';
+
+export default function Cistado() {
+  const [alumnos, setAlumnos] = useState([]);
+  const [nuevoAlumno, setNuevoAlumno] = useState({
+    nombre: "",
+    apellido: "",
+    dni: "",
+  });
+
+  const handleAgregarAlumno = () => {
+    const nuevosAlumnos = [...alumnos];
+    nuevosAlumnos.push(nuevoAlumno);
+    setAlumnos(nuevosAlumnos);
+
+    setNuevoAlumno({
       nombre: "",
       apellido: "",
       dni: "",
     });
+  };
 
-    const handleAgregarAlumno = () => {
-        const nuevosAlumnos = [...alumnos];
-        nuevosAlumnos.push(nuevoAlumno);
-        setAlumnos(nuevosAlumnos);
+  const handleEliminarAlumno = (index) => {
+    const nuevosAlumnos = [...alumnos];
+    nuevosAlumnos.splice(index, 1);
+    setAlumnos(nuevosAlumnos);
+  };
 
-        setNuevoAlumno({
-            nombre: "",
-            apellido: "",
-            dni: "",
-          });
-    };
-
-    const isCamposCompletos =
+  const isCamposCompletos =
     nuevoAlumno.nombre &&
     nuevoAlumno.apellido &&
     nuevoAlumno.dni;
 
-    return (
+  return (
+    <div>
+      <Nav Infoh1="Asistencia de Alumnos" />
+      <div className="listado">
         <div>
-            <Nav Infoh1="Asistencia de Alumnos"/>
-            <div className="listado">
-                <div >
-                    <h2 className="titulo">Crear Listado</h2>
-                    <table className="tabla">
-                        <thead>
-                            <tr>
-                                <th>Nombre</th>
-                                <th>Apellido</th>
-                                <th>D.N.I</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {alumnos.map((nota, index) => (
-                            <tr key={index}>
-                                <td>{nota.nombre}</td>
-                                <td>{nota.apellido}</td>
-                                <td>{nota.dni}</td>
-                            </tr>
-                        ))}
-                        <tr>
-                            <td>
-                                <input
-                                type="text"
-                                value={nuevoAlumno.nombre}
-                                onChange={(e) =>
-                                    setNuevoAlumno({ ...nuevoAlumno, nombre: e.target.value })
-                                }
-                                />
-                            </td>
-                            <td>
-                                <input
-                                type="text"
-                                value={nuevoAlumno.apellido}
-                                onChange={(e) =>
-                                    setNuevoAlumno({ ...nuevoAlumno, apellido: e.target.value })
-                                }
-                                />
-                            </td>
-                            <td>
-                                <input
-                                    type="text"
-                                    value={nuevoAlumno.dni}
-                                    onChange={(e) => {
-                                        const newValue = e.target.value;
-                                        if (/^\d{0,10}$/.test(newValue)) { // Validar que solo se ingresen números y como máximo 10 caracteres
-                                        setNuevoAlumno({ ...nuevoAlumno, dni: newValue });
-                                        }
-                                    }}
-                                    pattern="\d*"
-                                    maxLength="10"
-                                />
-                            </td>
-                            <td>
-                                <button
-                                onClick={handleAgregarAlumno}
-                                disabled={!isCamposCompletos}
-                                >
-                                +
-                                </button>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+          <h2 className="titulo">Crear Listado</h2>
+          <table className="tabla">
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>D.N.I</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {alumnos.map((alumno, index) => (
+                <tr key={index}>
+                  <td>{alumno.nombre}</td>
+                  <td>{alumno.apellido}</td>
+                  <td>{alumno.dni}</td>
+                  <td>
+                    <button onClick={() => handleEliminarAlumno(index)}>Eliminar</button>
+                  </td>
+                </tr>
+              ))}
+              <tr>
+                <td>
+                  <input
+                    type="text"
+                    value={nuevoAlumno.nombre}
+                    onChange={(e) =>
+                      setNuevoAlumno({ ...nuevoAlumno, nombre: e.target.value })
+                    }
+                  />
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    value={nuevoAlumno.apellido}
+                    onChange={(e) =>
+                      setNuevoAlumno({ ...nuevoAlumno, apellido: e.target.value })
+                    }
+                  />
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    value={nuevoAlumno.dni}
+                    onChange={(e) => {
+                      const newValue = e.target.value;
+                      if (/^\d{0,10}$/.test(newValue)) {
+                        setNuevoAlumno({ ...nuevoAlumno, dni: newValue });
+                      }
+                    }}
+                    pattern="\d*"
+                    maxLength="10"
+                  />
+                </td>
+                <td>
+                  <button
+                    onClick={handleAgregarAlumno}
+                    disabled={!isCamposCompletos}
+                  >
+                    +
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
