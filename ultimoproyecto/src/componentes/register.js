b7c1d84e534ef3b5895fa3db6da5d40c272457f6
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-10-24 11:10:38 -0300

    Register JS

--- ultimoproyecto/src/componentes/register.js
+++ ultimoproyecto/src/componentes/register.js
@@ -1,6 +1,13 @@
 import React, { Component, useEffect, useState } from "react";
 import ReactDOM from 'react-dom/client'
+import 'register.css'
 export default function Register () {
+    // Validar contraseña: al menos 6 caracteres, una mayúscula y un número
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d).{12,}$/;
+    if (!passwordRegex.test(password)) {
+      setPasswordError("La contraseña debe tener al menos 12 caracteres, una letra mayúscula y un número.");
+      return;
+    }
     return (
         <div>
 
