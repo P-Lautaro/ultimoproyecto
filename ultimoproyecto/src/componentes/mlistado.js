f8021eea90840fbf1dd4905b87742466bc657792
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-10-24 10:28:48 -0300

    Modificar Listado JS Firebase

--- ultimoproyecto/src/componentes/mlistado.js
+++ ultimoproyecto/src/componentes/mlistado.js
@@ -1,6 +1,7 @@
 import React, { Component, useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { Nav } from "./Nav/nav";
+import { getDatabase, ref, get } from "firebase/database";
 import './Modificar Listado/modifcarListado.css'
 
 export default function MListado (){
