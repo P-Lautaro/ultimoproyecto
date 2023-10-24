73ca1750001a02313e73669515135ed04ef7d967
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-10-24 11:05:32 -0300

    Register JS

---
+++ ultimoproyecto/src/componentes/register.js
@@ -0,0 +1,9 @@
+import React, { Component, useEffect, useState } from "react";
+import ReactDOM from 'react-dom/client'
+export default function Register () {
+    return (
+        <div>
+
+        </div>
+    )
+}
