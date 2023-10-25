2014b247f4bf97c62a36b329ee29e7319ca213f5
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-10-25 09:42:57 -0300

    Nav Img

--- ultimoproyecto/src/componentes/Nav/nav.jsx
+++ ultimoproyecto/src/componentes/Nav/nav.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import "./nav.css";
-
+import MyImage from "./epet20-PhotoRoom.png"
 export function Nav (props){
     return (
         <div className="barra">
+            <img src={MyImage}/>
             <h1 className="text">{props.Infoh1}</h1>
         </div>
       );
