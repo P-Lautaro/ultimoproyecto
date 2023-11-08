46b644f19c6630e94910dfd0d75ec5bd4d197a42
P-Lautaro <parritalauti@gmail.com>
2023-11-08 16:21:50 -0300

    modificando

--- ultimoproyecto/src/App.js
+++ ultimoproyecto/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import './App.css';
 import Login from './componentes/Login/login'; 
 import { useState, useEffect } from 'react';
-import HashLoader from "react-spinners/HashLoader";
 import { AuthProvider } from './context/AuthContext';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from './home';
